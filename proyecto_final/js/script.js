c51af74bd592c1b27ab1ac9787e22311cbfbe900
AA01-G02-1993 <rafaelarevalo6@gmail.com>
2018-09-06 11:57:03 -0600

    proyecto_final
    ingrese texto y imagen

---
+++ proyecto_final/js/script.js
@@ -0,0 +1,11 @@
+ //tomamos la altura   
+ var altura = $('nav').offset().top;
+ //verificamos el scroll
+ $(window).on('scroll', function(){
+     //si escroll es mayor a altura
+     if ( $(window).scrollTop() > altura ){
+         $('nav').addClass('NavFixed');
+     } else {
+         $('nav').removeClass('NavFixed');
+     }
+ });
