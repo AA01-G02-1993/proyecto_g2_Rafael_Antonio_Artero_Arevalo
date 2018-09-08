33b962cd3b04cc2b2c66b903be565f1203965ddd
AA01-G02-1993 <rafaelarevalo6@gmail.com>
2018-09-08 12:07:56 -0600

    proyecto_final
    agregue texto en el aside

--- proyecto_final/js/script.js
+++ proyecto_final/js/script.js
@@ -9,3 +9,27 @@
          $('nav').removeClass('NavFixed');
      }
  });
+
+ $("#ir-novedades").on("click", function (Evento) {
+    Evento.preventDefault();
+    $('html,body').animate({
+        scrollTop: $("#tema1").offset().top - 250
+    }, 500);
+});
+
+$("#ir-categorias").on("click", function (Evento) {
+    Evento.preventDefault();
+    $('html,body').animate({
+        scrollTop: $("#tema2").offset().top -20}, 500);
+});
+
+$("#ir-pie-pagina").on("click", function (Evento) {
+    Evento.preventDefault();
+    $('html,body').animate({
+        scrollTop: $("#pie_pagina").offset().top}, 1500);
+});
+
+$("body").on("click",".submenu a",function () {
+    $("li").removeClass('active').animate(200);
+    $(this).parent().addClass('active').animate(200);
+});
