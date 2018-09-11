73bdd8269bea78fb676a1eabdd4890acff8c8f9d
AA01-G02-1993 <rafaelarevalo6@gmail.com>
2018-09-11 14:37:42 -0600

    proyecto_final
    agregue el pie de pagina

--- proyecto_final/js/script.js
+++ proyecto_final/js/script.js
@@ -9,27 +9,51 @@
          $('nav').removeClass('NavFixed');
      }
  });
-
+// para ir a novedades
  $("#ir-novedades").on("click", function (Evento) {
     Evento.preventDefault();
     $('html,body').animate({
         scrollTop: $("#tema1").offset().top - 250
     }, 500);
 });
-
+// para ir a categorias
 $("#ir-categorias").on("click", function (Evento) {
     Evento.preventDefault();
     $('html,body').animate({
         scrollTop: $("#tema2").offset().top -20}, 500);
 });
-
-$("#ir-pie-pagina").on("click", function (Evento) {
+// para ir a pie de pagina
+$("#ir-pie_pagina").on("click", function (Evento) {
     Evento.preventDefault();
     $('html,body').animate({
         scrollTop: $("#pie_pagina").offset().top}, 1500);
 });
-
-$("body").on("click",".submenu a",function () {
-    $("li").removeClass('active').animate(200);
+// para que se desactive la actividad del nav al presionar otra opcion
+$("body").on("click",".menu a",function () {
+    $("li").removeClass('activo').animate(200);
     $(this).parent().addClass('active').animate(200);
+});
+ //efecto al pasar sobre los temas
+$(document).ready(function(){
+    $(".efecto").hover(function(){
+        $(this).css("color", "gray");
+        }, function(){
+        $(this).css("color", "black");
+    });
+});
+//efecto al pasar sobre los temas
+$(document).ready(function(){
+    $("h2").hover(function(){
+        $(this).css("color", "gray");
+        }, function(){
+        $(this).css("color", "black");
+    });
+});
+//efecto al pasarsobre una imagen
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
 });
